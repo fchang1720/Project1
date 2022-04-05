ad6ae156b4b6e173c386b24d536750dd79d7525c
Brady Mavetz <b.mavetz@outlook.com>
2022-04-05 13:50:27 -0500

    saving changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 var apiKey = '&appid=8710c92cc91b2be9b69b111ac287d778';
 var currWeather = 'https://api.openweathermap.org/data/2.5/weather?q=';
+var cityInput = $('#textarea1');
 var lat;
 var lon;
 var date = moment().format('YYYY-MM-DD');
+var datePlus = moment().add(1, 'days').format('YYYY-MM-DD');
 console.log(date);
-
+console.log(datePlus);
 $('#submit-btn').on('click', function(){
   var calendarDate = $('.datepicker').val();
-  
+  console.log(cityInput.val());
   console.log(calendarDate);
   if(date == calendarDate){
     console.log('true');
   }
+  if(calendarDate == '' || cityInput.val() == ''){
+    console.log('empty');
+  }else{
+    getLatLon();
+  }
 });
-getLatLon();
+
 function getLatLon() {
-var currentWeathURL = (currWeather + 'Boston' + apiKey + '&units=imperial');
+var currentWeathURL = (currWeather + cityInput.val() + apiKey + '&units=imperial');
     fetch(currentWeathURL)
     .then(function (response) {
       return response.json();
@@ -37,6 +44,6 @@ var currentWeathURL = (currWeather + 'Boston' + apiKey + '&units=imperial');
     .then(function (data) {
       
       console.log(data);
-    
+      
     })
   } 
